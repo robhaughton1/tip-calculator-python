abc0f456c46a409562e7f2cfc93178403d0eb9e3
robhaughton1 <152531780+robhaughton1@users.noreply.github.com>
2026-02-10 23:50:20 -0500

    Fix input validation and update print statement

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # Asks the meal price
 meal_price = float(input("What is the meal price?")) 
 
+if meal_price <=0:
+  print("Invalid meal price.")
+  exit()
+
 # Asks the tip price
 tip_price = float(input("How much will you tip?"))
+if tip_price < 0:
+  print("Positive numbers only.")
 
 # Calculates the tip
 tip_percent = tip_price / 100 * meal_price
@@ -10,4 +16,4 @@ tip_percent = tip_price / 100 * meal_price
 # Calculates the total of the entire meal + tip
 meal_total = round((meal_price + tip_percent), 2)
 
-print("Your total is: " + "$" + str(meal_total))
+print(f"Your total is: {str(meal_total)}.")
